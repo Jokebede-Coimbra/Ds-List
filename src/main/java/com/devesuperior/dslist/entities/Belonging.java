f7d690ccaf87d31af83d5231946a0b609ad3a25a
Coimbra <adsjkbd@gmail.com>
2023-07-13 14:00:29 -0300

    Domain model

---
+++ src/main/java/com/devesuperior/dslist/entities/Belonging.java
@@ -0,0 +1,56 @@
+package com.devesuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    // Classe auxiliar para representar uma chave primária multipla, pq o repository só recebe uma chave
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging() {
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        this.id.setGame(game);
+        this.id.setList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Belonging)) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(getId(), belonging.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
